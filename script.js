bd43c9379a97430b411f72c4408da4df3cc0d4f3
chahrazad <chahrazadkhatiri.2@gmail.com>
2022-06-23 18:30:11 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,51 @@
+/*
+I : sentence
+O : -The number of vowels in the sentence
+    -The length of the sentence
+    -The number of words in the sentence
+C :
+E : - The sentence must end with a point
+    - The length of the sentence without space
+    - The number of words in the sentence without ".",","
+ */
+
+
+let sentence = "write here a sentence";
+let sentenceLastChar = sentence[sentence.length - 1];
+let word = sentence.split(" ");
+let joinWord = word.join();
+let char = joinWord.split("");
+
+let numberOfVowels = 0;
+let lengthOfSentence = 0;
+let numberOfWords = 0;
+
+if (sentenceLastChar == ".") {
+  //The number of vowels in the sentence
+  for (let i = 0; i < char.length; i++) {
+    if (
+      char[i] == "a" ||
+      char[i] == "e" ||
+      char[i] == "i" ||
+      char[i] == "o" ||
+      char[i] == "u"
+    ) {
+      numberOfVowels += 1;
+    }
+  }
+  console.log("The number of vowels in the sentence :", numberOfVowels);
+  //The length of the sentence
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] == " ") continue;
+    lengthOfSentence += 1;
+  }
+  console.log("The length of the sentence :", lengthOfSentence);
+  //The number of words in the sentence
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == "." || word[i] == ",") continue;
+    numberOfWords += 1;
+  }
+  console.log("The number of words in the sentence  :", numberOfWords);
+} else {
+  console.log("the sentence must end with a point");
+}
